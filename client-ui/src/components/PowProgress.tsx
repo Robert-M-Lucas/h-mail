6e48b6fe61bccb4b6e68d382fe55932ffe98e974
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-08-21 16:05:13 +0100

    Update

--- client-ui/src/components/PowProgress.tsx
+++ client-ui/src/components/PowProgress.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, ReactNode, SetStateAction, useState } from "react"
 import { listen } from "@tauri-apps/api/event"
-import { Modal, ProgressBar } from "react-bootstrap"
+import { Button, Modal, ProgressBar } from "react-bootstrap"
 import { invoke } from "@tauri-apps/api/core"
 
 interface Props {
@@ -43,24 +43,30 @@ export default function PowProgress({ children }: Props) {
 
   return (
     <div>
-      <Modal
-        show={powProgress !== undefined}
-        centered
-        size="lg"
-        onHide={async () => await invoke("cancel_current_pow")}
-      >
-        <Modal.Header closeButton>
+      <Modal show={powProgress !== undefined} centered size="lg">
+        <Modal.Header>
           <Modal.Title>Solving Proof-of-Work</Modal.Title>
         </Modal.Header>
         {powProgress && (
-          <Modal.Body>
-            <ProgressBar
-              animated
-              now={(powProgress.progress / powProgress.out_of) * 100}
-              label={`${powProgress.progress.toLocaleString("en-US")} / ${powProgress.out_of.toLocaleString("en-US")}`}
-            />
-            <div className="w-100 text-center">{powProgress.text}</div>
-          </Modal.Body>
+          <>
+            <Modal.Body>
+              <ProgressBar
+                animated
+                now={(powProgress.progress / powProgress.out_of) * 100}
+                label={`${powProgress.progress.toLocaleString("en-US")} / ${powProgress.out_of.toLocaleString("en-US")}`}
+              />
+              <div className="w-100 text-center">{powProgress.text}</div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                className={"w-100"}
+                variant={"danger"}
+                onClick={async () => await invoke("cancel_current_pow")}
+              >
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </>
         )}
       </Modal>
       {children}
