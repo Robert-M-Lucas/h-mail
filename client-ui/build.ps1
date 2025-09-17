4b2563fc506150769634d62c0ee48ec94880396f
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-09-17 20:58:23 +0100

    Update

--- client-ui/build.ps1
+++ client-ui/build.ps1
@@ -1,12 +1,28 @@
 $ErrorActionPreference = "Stop"
 
 Set-Location -Path "builds"
-
 Get-ChildItem -Filter "*.msi" | Remove-Item -Force
 
 Set-Location -Path ".."
-
 npm run tauri build
 
-Copy-Item -Path "../target/release/bundle/msi/client-ui_0.1.0_x64_en-US.msi" -Destination "builds/client-ui_0.1.0_x64_en-US.msi" -Force
+$msiPath = Get-ChildItem "../target/release/bundle/msi" -Filter "client-ui_*_x64_en-US.msi" |
+    ForEach-Object {
+        if ($_ -match 'client-ui_(\d+\.\d+\.\d+)_x64_en-US\.msi') {
+            [PSCustomObject]@{
+                File    = $_
+                Version = [version]$matches[1]
+            }
+        }
+    } |
+    Sort-Object Version -Descending |
+    Select-Object -First 1
 
+if ($null -eq $msiPath) {
+    Write-Error "No MSI package found!"
+    exit 1
+}
+
+Write-Host "Using client-ui version:" $msiPath.Version
+
+Copy-Item -Path $msiPath.File.FullName -Destination ("builds/" + $msiPath.File.Name) -Force
--- client-ui/build.sh
+++ client-ui/build.sh
@@ -1,8 +1,14 @@
 cd builds &&
- rm client-ui &&
- rm *.deb &&
- cd .. &&
- npm run tauri build &&
- cp ../target/release/bundle/deb/client-ui_0.1.0_amd64/data/usr/bin/client-ui client-ui &&
- cp ../target/release/bundle/deb/client-ui_0.1.0_amd64/data/usr/bin/client-ui builds/client-ui &&
- cp ../target/release/bundle/deb/client-ui_0.1.0_amd64.deb builds/client-ui_0.1.0_amd64.deb
+rm -f client-ui &&
+rm -f *.deb &&
+cd .. &&
+npm run tauri build &&
+
+VERSION=$(ls ../target/release/bundle/deb/ | grep -oP 'client-ui_\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1) &&
+echo "Found build version $VERSION" &&
+
+{
+  cp ../target/release/bundle/deb/client-ui_${VERSION}_amd64/data/usr/bin/client-ui client-ui &&
+  cp ../target/release/bundle/deb/client-ui_${VERSION}_amd64/data/usr/bin/client-ui builds/client-ui &&
+  cp ../target/release/bundle/deb/client-ui_${VERSION}_amd64.deb builds/client-ui_${VERSION}_amd64.deb
+}
